bc1803867961b3aed2bba08d551f92b195e2b735
Nikhil <hk5561111212@gmail.com>
2025-06-23 21:40:58 +0530

    First Commit

---
+++ app/components/global/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className='bg-[#FAF7F2] py-2'>
+         <div className='container gap-2 hidden md:flex md:justify-between md:items-center'>
+
+              <div className='flex gap-4'>
+                 <Link className='text-[#4E342E] text-sm' href={"/"}>Home</Link>
+                 <Link className='text-[#4E342E] text-sm' href={"/shop"}>Shop All</Link>
+                 <Link className='text-[#4E342E] text-sm' href={"/about"}>About</Link>
+              </div>
+              <Link className='text-[#4E342E] text-4xl' href="/">SweetSimmooons</Link>
+              <div className='flex gap-4'>
+                <Link className='text-[#4E342E] text-sm' href={"/contact"}>Contact Us</Link>
+                <Link className='text-[#4E342E] text-sm' href={"/account"}>Account</Link>
+                <Link className='text-[#4E342E] text-sm' href={"/cart"}>Cart</Link>
+              </div>
+         </div>
+    </header>
+  )
+}
+
+function MobileMenu(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Header
