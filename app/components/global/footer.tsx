5187659e04a3c69d77f623ddda49dbc9d440adae
Nikhil <hk5561111212@gmail.com>
2025-06-25 23:38:17 +0530

    Added Footer

---
+++ app/components/global/footer.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer className='pt-20 pb-8 bg-white'>
+        <div className='px-20 flex flex-col gap-14'>
+            <div className='flex justify-between'>
+            <div className='relative'>
+                <h2 className='text-black text-4xl mb-4 italic font-extralight'>Join the legacy club</h2>
+                <p className='text-black text-lg'>sign up to be the first to know about new arrivals & exclusive offers.</p>
+                <form className='w-full flex items-center h-14 mt-3.5'>
+                    <input className='bg-[#DDDAD4] w-full h-full placeholder:text-black placeholder:uppercase px-4' type='email' placeholder='Enter E-mail Address' />
+                    <button className='text-black h-full px-6 uppercase bg-[#DDDAD4]'>Submit</button>
+                </form>
+            </div>
+            <div className='flex gap-20'>
+                <ul>
+                    <h3 className='text-xl text-black mb-2'>INFO</h3>
+                    <li className='text-black text-lg'>Account</li>
+                    <li className='text-black text-lg'>Privacy Policy</li>
+                </ul>
+                <ul>
+                    <h3 className='text-xl text-black mb-2'>SUPPORT</h3>
+                    <li className='text-black text-lg'>Contact</li>
+                    <li className='text-black text-lg'>FAQ</li>
+                </ul>
+                 <ul>
+                    <h3 className='text-xl text-black mb-2'>CONNECT</h3>
+                    <li className='text-black text-lg'>Instagram</li>
+                    <li className='text-black text-lg'>X</li>
+                </ul>
+            </div>
+            </div>
+            <div>
+                <Image src={'/images/footer-images/svgviewer-png-output.png'} height={500} width={1920} alt='logo' />
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
--- app/components/sections/testimonials.tsx
+++ app/components/sections/testimonials.tsx
@@ -45,14 +45,12 @@ const Testimonials = () => {
   };
 
   return (
-    <section className="py-12 bg-white">
+    <section className="py-12 bg-[#F6F4F0]">
       <div className="max-w-4xl mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-8">Customer Testimonials</h2>
-
         <Splide options={options} aria-label="Customer Testimonials">
           {testimonials.map((item, i) => (
             <SplideSlide key={i}>
-              <div className="bg-gray-50 shadow-md p-6 rounded-xl text-center flex flex-col gap-4 h-full justify-between">
+              <div className="shadow-md p-6 rounded-xl text-center flex flex-col gap-4 h-full justify-between">
                 <h2 className="text-gray-700 italic text-3xl">“{item.feedback}”</h2>
                 <h4 className="font-semibold text-md text-orange-600">— {item.name}</h4>
               </div>
