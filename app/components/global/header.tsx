ff10f7ed9b1936c584c4529faaf6bf50b471fd43
Nikhil <hk5561111212@gmail.com>
2025-07-02 01:04:17 +0530

    Updated the header

--- app/components/global/header.tsx
+++ app/components/global/header.tsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Search, User, ShoppingCart } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className='bg-[#FAF7F2] py-2 z-10 relative'>
-         <div className='container gap-2 hidden md:flex md:justify-between md:items-center'>
-
-              <div className='flex gap-4'>
-                 <Link className='text-[#4E342E] text-sm' href={"/"}>Home</Link>
-                 <Link className='text-[#4E342E] text-sm' href={"/collections/all"}>Shop All</Link>
-                 <Link className='text-[#4E342E] text-sm' href={"/about"}>About</Link>
-              </div>
-              <Link className='text-[#4E342E] text-4xl' href="/">SweetSimmooons</Link>
-              <div className='flex gap-4'>
-                <Link className='text-[#4E342E] text-sm' href={"/contact"}>Contact Us</Link>
-                <Link className='text-[#4E342E] text-sm' href={"/account"}>Account</Link>
-                <Link className='text-[#4E342E] text-sm' href={"/cart"}>Cart</Link>
-              </div>
-         </div>
-    </header>
-  )
-}
-
-function MobileMenu(){
-    return(
-        <div>
-
+    <header className="bg-white py-2 z-10 relative">
+      <div className="container gap-2 hidden md:flex md:justify-between md:items-center">
+        <div className="flex gap-4">
+          <Link
+            className="text-[#4E342E] text-sm uppercase"
+            href={"/collections/all"}
+          >
+            Shop
+          </Link>
+          <Link className="text-[#4E342E] text-sm uppercase" href={"/about"}>
+            Discover
+          </Link>
         </div>
-    )
-}
+        <Link className="text-[#4E342E] text-4xl" href="/">
+          <Image src="/images/logo.png" alt="logo" height={40} width={128} />
+        </Link>
+        <div className="flex gap-4">
+          <Search size={20} strokeWidth={0.8} />
+          <Link className="text-[#4E342E] text-sm" href={"/account"}>
+            <User size={20} strokeWidth={0.8} />
+          </Link>
+          <Link className="text-[#4E342E] text-sm" href={"/cart"}>
+            <ShoppingCart size={20} strokeWidth={0.8} />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+function MobileMenu() {
+  return <div></div>;
+}
+export default Header;
