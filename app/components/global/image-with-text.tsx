a89705b2c5b7f463b7588d8fae302db2a50f8f6a
Nikhil <hk5561111212@gmail.com>
2025-07-08 23:15:48 +0530

    Added text with image section

---
+++ app/components/global/image-with-text.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  imageUrl: string;
+  heading: string;
+  subheading: string | React.ReactNode;
+  buttonUrl: string;
+  imagePosition: "left" | "right";
+};
+
+const ImageWithText = ({
+  imageUrl,
+  heading,
+  subheading,
+  buttonUrl,
+  imagePosition = "left",
+}: Props) => {
+  return (
+    <section className="w-screen">
+      <div
+        className={`${
+          imagePosition === "right" ? "flex-row-reverse" : "flex-row"
+        } flex items-center justify-center`}
+      >
+        <div className="flex-1">
+          <Image src={imageUrl} height={1000} width={1000} alt="Image" />
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="w-lg">
+            <h3 className="text-[42px] leading-[46px]">{heading}</h3>
+            <p className="text-sm leading-4.5 mt-4">{subheading}</p>
+            <Link
+              className="mt-6 block w-fit uppercase underline"
+              href={buttonUrl}
+            >
+              Read More
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ImageWithText;
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import InfoImageBanner from "./components/sections/info-banner";
 import QuoteSection from "./components/sections/quote-section";
 import VideoBanner from "./components/sections/video-bannner";
 import Testimonials from "./components/sections/testimonials";
+import ImageWithText from "./components/global/image-with-text";
 
 export default function Home() {
   return (
@@ -12,6 +13,28 @@ export default function Home() {
       <Banner />
       <PressSplide />
       <Products />
+      <ImageWithText
+        heading="CLEAN, GENTLE + RESULTS-DRIVEN"
+        subheading="Aevi is made with pure, therapeutic ingredients from Nordic nature. Our performance-driven formulas are clinically tested to ensure efficacy and visible results while still being gentle for sensitive skin."
+        imageUrl={`/images/Aevi-Web-Homepage-Square-01.webp`}
+        buttonUrl=""
+        imagePosition="left"
+      />
+      <ImageWithText
+        heading="FOR SENSITIVE SKIN, BY SENSITIVE SKIN"
+        subheading={
+          <>
+            Aevi is dermatologically tested and suitable for even the most
+            sensitive skin. <br />
+            Our skincare is expertly formulated to soothe, hydrate, nourish and
+            strengthen the skin because we believe building healthy, resilient
+            skin is the key to naturally radiant beauty and well-ageing.
+          </>
+        }
+        imageUrl={`/images/Aevi-Web-Homepage-Square-03.webp`}
+        buttonUrl=""
+        imagePosition="right"
+      />
       <InfoImageBanner />
       <QuoteSection />
       <VideoBanner />
