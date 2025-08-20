af2267dc731722c65a7895169952d8601ed81416
Nikhil <hk5561111212@gmail.com>
2025-08-20 23:59:46 +0530

    fixed products/[slug] route

--- app/api/products/[slug]/route.ts
+++ app/api/products/[slug]/route.ts
@@ -1,17 +1,21 @@
 import { PrismaClient } from "@/app/generated/prisma";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function GET(
   _req: NextRequest,
-  context: { params: { slug: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = context.params;
+  const { slug } = await params;
 
   const product = await prisma.product.findUnique({
     where: { slug },
   });
 
-  return Response.json({ product });
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ product });
 }
